21dbe3eb0f1b8139f0312adc95ecbaf912f007eb
Makthala Sai Teja Goud <mstg1102@gmail.com>
2025-01-03 12:00:34 +0530

    005

--- 05.Patterns I/D10_Question6.java
+++ 05.Patterns I/D10_Question6.java
@@ -14,10 +14,21 @@
 public class D10_Question6 {
     public static void main(String[] args){
         int n = 8;
-        for(int line = 1;line <= n;line++){
+        for(int line = 1;line <= n/2;line++){
             for(int star = 1; star <= n;star++){
-                
-                if(star <= line){
+
+                if(star <= line || star >= n-line+1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for(int line2 = (n/2);line2 >= 1;line2--){
+            for(int star2 = n;star2 >= 1;star2--){
+                if(star2 <= line2 || star2 >= n-line2+1){
                     System.out.print("* ");
                 }
                 else{
@@ -30,4 +41,5 @@ public class D10_Question6 {
 }
 
 
-// still work in progress!!!!!!!!!!!!!
+// Only working for even numbers still working on it 
+// it will be updated soon
